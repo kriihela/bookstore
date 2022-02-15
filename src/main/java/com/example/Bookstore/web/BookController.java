39880438716d9b2d41750627a080a6a07f0f6b8b
Kristian Riihela <kristian.riihela@gmail.com>
2022-02-15 19:06:59 +0200

    create and delete functionalities added

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 
 @Controller
@@ -17,5 +20,20 @@ public class BookController {
 	public String getBook(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+	@GetMapping("/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	@PostMapping("/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+		repository.deleteById(id);
+		return "redirect:../booklist";
 	}
 }

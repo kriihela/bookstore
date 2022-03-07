627a3fdf0e251b9461a68774d74542b800bc3708
Kristian Riihela <kristian.riihela@gmail.com>
2022-03-07 22:10:09 +0200

    customized spring REST

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,15 +1,20 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.CategoryRepository;
+
 
 @Controller
 public class BookController {
@@ -24,6 +29,15 @@ public class BookController {
 	public String getBook(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+	@GetMapping("/books")
+	public @ResponseBody List<Book> bookListRest() {	
+		return (List<Book>) repository.findAll();
+	}
+	@GetMapping("/books/{id}")
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {
+		return repository.findById(id);
+		
 	}
 	@GetMapping("/add")
 	public String addBook(Model model) {
